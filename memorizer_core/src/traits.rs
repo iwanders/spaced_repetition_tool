1958996ad19e11698bc1c841453fdf69c46bed6f
Ivor Wanders <ivor@iwanders.net>
2022-06-19 08:55:53 -0400

    Add ids.

--- memorizer_core/src/traits.rs
+++ memorizer_core/src/traits.rs
@@ -1,24 +1,39 @@
-
-
-pub enum RepresentationType
-{
+pub enum RepresentationType {
     Text,
 }
 
 /// A partciular representation.
-pub trait Representation : std::fmt::Debug{
+pub trait Representation: std::fmt::Debug {
+    /// Get the type of this presentation.
     fn get_type(&self) -> RepresentationType;
+
+    /// Get the textual representation.
     fn get_text(&self) -> &str;
+
+    /// Unique id for this representation.
+    fn get_id(&self) -> u128;
 }
 
 /// A transformation, like Hex->Binary or 'Translate from A into B'
-pub trait Transformation : std::fmt::Debug {
+pub trait Transformation: std::fmt::Debug {
     /// A string describing the particular transformation to be performed.
     fn get_description(&self) -> &str;
+
+    /// Unique id for this Transformation.
+    fn get_id(&self) -> u128;
 }
 
 /// Something that relates transformations and representations to each other.
-pub trait Learnable : std::fmt::Debug {
-    fn get_edges(&self) -> &[(&dyn Representation, &dyn Transformation, &dyn Representation)];
-}
+pub trait Learnable: std::fmt::Debug {
+    /// Get the possible edges for this learnable.
+    fn get_edges(
+        &self,
+    ) -> &[(
+        &dyn Representation,
+        &dyn Transformation,
+        &dyn Representation,
+    )];
 
+    /// Unique id for this learnable.
+    fn get_id(&self) -> u128;
+}

207aa9f8cdc206cc4671348c000843edbe13851e
Ivor Wanders <ivor@iwanders.net>
2022-08-01 10:27:21 -0400

    Add all the unprintable ascii characters.

--- examples/generate_number_representation.rs
+++ examples/generate_number_representation.rs
@@ -170,8 +170,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (1, "SOH"),
             (2, "STX"),
             (3, "ETX"),
+            (4, "EOT"),
+            (5, "ENQ"),
+            (6, "ACK"),
+            (7, "BEL"),
+            (8, "BS"),
+            (9, "HT"),
             (10, "LF"),
+            (11, "VT"),
+            (12, "FF"),
             (13, "CR"),
+            (14, "SO"),
+            (15, "SI"),
+            (16, "DLE"),
+            (17, "DC1"),
+            (18, "DC2"),
+            (19, "DC3"),
+            (20, "DC4"),
+            (21, "NAK"),
+            (22, "SYN"),
+            (23, "ETB"),
+            (24, "CAN"),
+            (25, "EM"),
+            (26, "SUB"),
+            (27, "ESC"),
+            (28, "FS"),
+            (29, "GS"),
+            (30, "RS"),
+            (31, "US"),
+            (127, "DEL"),
         ]);
         if let Some(v) = unprintables.get(&value) {
             return TextRepresentation::new(v, RepresentationId(value + ASCII_SHIFT));
@@ -188,10 +215,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Ok... so this is a bit tricky.
         // 0 to 32 are the unprintables, some of them are still useful, like \n and \r.
         // 32 itself is a space.
-        if (value <= 4 || value == 10 || value == 13) || (value >= 32 && value <= 126) {
-            return true;
-        }
-        false
+        // if (value <= 4 || value == 10 || value == 13) || (value >= 32 && value <= 126) {
+        // return true;
+        // }
+        // false
+        value <= 127 // everything below 127 is now valid ascii.
     }
 
     let learnable_id_base: u64 = 1656462468 << 32; // Totally legit unique number!
